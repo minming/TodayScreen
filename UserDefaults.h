8d35ec4dc9edcae559dd1611e139a41971b0174c
Shravan Reddy <ShravanReddy@rescomp-08-107792.Stanford.EDU>
2009-05-25 22:09:52 -0700

    comment

---
+++ UserDefaults.h
@@ -0,0 +1,16 @@
+//
+//  UserDefaults.h
+//  TodayScreen
+//
+//  Created by Shravan Reddy on 5/25/09.
+//  Copyright 2009 Stanford University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface UserDefaults : NSObject {
+
+}
+
+@end
---
+++ UserDefaults.m
@@ -0,0 +1,14 @@
+//
+//  UserDefaults.m
+//  TodayScreen
+//
+//  Created by Shravan Reddy on 5/25/09.
+//  Copyright 2009 Stanford University. All rights reserved.
+//
+
+#import "UserDefaults.h"
+
+
+@implementation UserDefaults
+
+@end
