22679d082c1fe4137928fc0f916e182038de2307
Shravan Reddy <ShravanReddy@rescomp-08-106567.Stanford.EDU>
2009-05-26 21:20:11 -0700

    Added UserDefaults Implementation

--- TodayScreenTableViewController.m
+++ TodayScreenTableViewController.m
@@ -42,40 +42,40 @@
 	
 	
 	//self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"bg1.jpg"]];
-
+	
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 
 /*
-- (void)viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
-}
-*/
+ - (void)viewWillAppear:(BOOL)animated {
+ [super viewWillAppear:animated];
+ }
+ */
 /*
-- (void)viewDidAppear:(BOOL)animated {
-    [super viewDidAppear:animated];
-}
-*/
+ - (void)viewDidAppear:(BOOL)animated {
+ [super viewDidAppear:animated];
+ }
+ */
 /*
-- (void)viewWillDisappear:(BOOL)animated {
-	[super viewWillDisappear:animated];
-}
-*/
+ - (void)viewWillDisappear:(BOOL)animated {
+ [super viewWillDisappear:animated];
+ }
+ */
 /*
-- (void)viewDidDisappear:(BOOL)animated {
-	[super viewDidDisappear:animated];
-}
-*/
+ - (void)viewDidDisappear:(BOOL)animated {
+ [super viewDidDisappear:animated];
+ }
+ */
 
 /*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-*/
+ // Override to allow orientations other than the default portrait orientation.
+ - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+ // Return YES for supported orientations
+ return (interfaceOrientation == UIInterfaceOrientationPortrait);
+ }
+ */
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning]; // Releases the view if it doesn't have a superview
@@ -103,38 +103,32 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     if (cell == nil) {
         cell = [[[UITableViewCell alloc] initWithFrame:CGRectZero reuseIdentifier:CellIdentifier] autorelease];
+		[cell setSelectionStyle:UITableViewCellSelectionStyleNone];
     }
     // Set up the cell...
-	if (indexPath.row == 5) {
-		WidgetAppLauncher *appLauncher = [[WidgetAppLauncher alloc] init];//WithNibName:@"WidgetAppLauncher.xib" bundle:nil] autorelease];
-		[cell.contentView addSubview:appLauncher.view];
-		[self.widgetsArray addObject:appLauncher];
-		[appLauncher release];
-	} 
-	
 	if (indexPath.row == 0) {
-		WidgetFlipClockDate *flipClockDate = [[WidgetFlipClockDate alloc] init];//WithNibName:@"WidgetAppLauncher.xib" bundle:nil] autorelease];
+		WidgetFlipClockDate *flipClockDate = [[WidgetFlipClockDate alloc] init];	//WithNibName:@"WidgetAppLauncher.xib" bundle:nil] autorelease];
 		[cell.contentView addSubview:flipClockDate.view];
 		[self.widgetsArray addObject:flipClockDate];
 		[flipClockDate release];
 	} 
 	
 	if (indexPath.row == 1) {
-		WidgetClockDate *clockDate = [[WidgetClockDate alloc] init];//WithNibName:@"WidgetAppLauncher.xib" bundle:nil] autorelease];
+		WidgetClockDate *clockDate = [[WidgetClockDate alloc] init];	//WithNibName:@"WidgetAppLauncher.xib" bundle:nil] autorelease];
 		[cell.contentView addSubview:clockDate.view];
 		[self.widgetsArray addObject:clockDate];
 		[clockDate release];
 	} 
 	
 	if (indexPath.row == 2) {
-		WidgetRSS *RSS = [[WidgetRSS alloc] init];//WithNibName:@"WidgetAppLauncher.xib" bundle:nil] autorelease];
+		WidgetRSS *RSS = [[WidgetRSS alloc] init];	//WithNibName:@"WidgetAppLauncher.xib" bundle:nil] autorelease];
 		[cell.contentView addSubview:RSS.view];
 		[self.widgetsArray addObject:RSS];
 		[RSS release];
 	} 
 	
 	if (indexPath.row == 3) {
-		WidgetWeather *weather = [[WidgetWeather alloc] init];//WithNibName:@"WidgetAppLauncher.xib" bundle:nil] autorelease];
+		WidgetWeather *weather = [[WidgetWeather alloc] init];	//WithNibName:@"WidgetAppLauncher.xib" bundle:nil] autorelease];
 		[cell.contentView addSubview:weather.view];
 		[self.widgetsArray addObject:weather];
 		[weather release];
@@ -146,7 +140,13 @@
 		[self.widgetsArray addObject:contact];
 		[contact release];
 	} 
-
+	
+	if (indexPath.row == 5) {
+		WidgetAppLauncher *appLauncher = [[WidgetAppLauncher alloc] init];//WithNibName:@"WidgetAppLauncher.xib" bundle:nil] autorelease];
+		[cell.contentView addSubview:appLauncher.view];
+		[self.widgetsArray addObject:appLauncher];
+		[appLauncher release];
+	} 
     return cell;
 }
 
@@ -157,51 +157,58 @@
 }  
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+	//return nil;
     // Navigation logic may go here. Create and push another view controller.
 	// AnotherViewController *anotherViewController = [[AnotherViewController alloc] initWithNibName:@"AnotherView" bundle:nil];
 	// [self.navigationController pushViewController:anotherViewController];
 	// [anotherViewController release];
 }
 
+- (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath 
+{ 
+	return nil;
+}
 
-/*
+- (BOOL)tableView:(UITableView *)tableView canSelectRowAtIndexPath:(NSIndexPath *)indexPath 
+{ 
+	return NO;
+}
+
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+	// Return NO if you do not want the specified item to be editable.
+	return NO;
 }
-*/
 
 
 /*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:YES];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
+ // Override to support editing the table view.
+ - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+ 
+ if (editingStyle == UITableViewCellEditingStyleDelete) {
+ // Delete the row from the data source
+ [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:YES];
+ }   
+ else if (editingStyle == UITableViewCellEditingStyleInsert) {
+ // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+ }   
+ }
+ */
+
+/*
+ // Override to support rearranging the table view.
+ - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+ }
+ */
 
 
 /*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
+ // Override to support conditional rearranging of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+ // Return NO if you do not want the item to be re-orderable.
+ return YES;
+ }
+ */
 
 
 - (void)dealloc {
